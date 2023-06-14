6bf09411e26b34b44202decd7c939bca5560c5f4
hiagocelestino <hiago.celestino@hotmail.com>
2023-06-13 23:35:02 -0300

    adicionando lib de testes unitarios, criando testes unitarios e atualizando classes

--- classes/Aeronave.php
+++ classes/Aeronave.php
@@ -1,50 +1,39 @@
 <?php
 declare(strict_types=1);
 include_once('Persiste.php');
+include_once('Veiculo.php');
 class Aeronave extends Veiculo{
-    private float $capacidadeCarga;
-    private string $registro;
-    static private $filename = 'aeronave.txt';
+  private float $capacidadeCarga;
+  private string $registro;
+  static private $filename = 'aeronave.txt';
+
+  public function __construct(string $fabricante, string $modelo, int $capacidadePassageiros, CiaAerea $proprietaria, float $capacidadeCarga, string $registro) {   
+    parent::__construct($fabricante, $modelo, $capacidadePassageiros, $proprietaria);  
+    $this->capacidadeCarga = $capacidadeCarga;
+    $this->setRegistro($registro);
+  }
   
-    public function __construct(string $fabricante, string $modelo, int $capacidadePass, CiaAerea $proprietaria, float $capacidadeCarga, string $registro) {   
-      parent::__construct($fabricante, $modelo, $capacidadePass, $proprietaria);  
-      $this->capacidadeCarga = $capacidadeCarga;
-        if($this->validaRegistro($registro)){
-          $this->registro = $registro;
-        }
-    }
-    
-    static public function getFilename(){
-      return get_called_class()::$filename;
+  static public function getFilename(){
+    return get_called_class()::$filename;
+  }
+
+  public function getCapacidadeCarga() {
+    return $this->capacidadeCarga;
+  }
+  
+  public function getRegistro() {
+    return $this->registro;
+  }
+
+  private function setRegistro(string $registro){
+    $pattern = '/((PT)|(PP)|(PS))(-)[A-Z]{3}$/';
+    if (!preg_match($pattern, $registro)){
+      throw new Exception('Registro inválido.');
     }
 
-    public function getCapacidadeCarga() {
-      return $this->capacidadeCarga;
-    }
-    
-    public function getRegistro() {
-      return $this->registro;
-    }
-
-    private function validaRegistro(string $registro) {
-      if (strlen($registro) != 6 || $registro[2] != '-') {  
-        return false;
-      }
-      $prefixo = substr($registro, 0, 2);
-      if ($prefixo != 'PT' && $prefixo != 'PR' && $prefixo != 'PP' && $prefixo != 'PS') {
-          echo 'Aeronave não permitida para voos nacionais.';
-          return false;
-      }
-      if (!ctype_upper($registro[0]) || !ctype_upper($registro[1])) {  
-        return false;
-      }
-      if (!ctype_upper($registro[3]) || !ctype_upper($registro[4]) || !ctype_upper($registro[5])) {  
-        return false;
-      }
-      return true;
+    $this->registro = $registro;
   }
 
 }
-
 
 ?>
